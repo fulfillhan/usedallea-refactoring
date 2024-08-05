61df548041d2908a0690c31215abf19ae33286ef
sanghui <tkdgml719@gamil.com>
2024-08-05 21:16:34 +0900

    [WIP] 사용자 인증 리팩토링

--- src/main/java/com/application/usedallea/member/v2/service/UserService.java
+++ src/main/java/com/application/usedallea/member/v2/service/UserService.java
@@ -11,7 +11,7 @@ public interface UserService{
 
     void deactivateUser(String userId);
 
-    boolean loginUser(UserRegisterDto userRegisterDto);
+    boolean validateUser(UserRegisterDto userRegisterDto);
 
     boolean checkDuplicatedUser(String userId);
 }
